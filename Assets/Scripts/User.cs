3c67d08c18971e28e701d0ab71b99ffbd86fccfc
MauroJrDeveloper <59150284+MauroJrDeveloper@users.noreply.github.com>
2020-10-13 09:15:23 -0300

    Project

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+
+    float _Speed_X = 5;
+    float _Speed_Y = 5;
+
+    Rigidbody2D _Rdb;
+
+    Vector2 _Up_Vector;
+    Vector2 _Down_Vector;
+    Vector2 _Left_Vector;
+    Vector2 _Right_Vector;
+
+    void Start()
+    {
+        _Rdb = GetComponent<Rigidbody2D>();
+        _Up_Vector = new Vector2(0, _Speed_Y);
+        _Down_Vector = new Vector2(0, -_Speed_Y);
+        _Left_Vector = new Vector2(-_Speed_X, 0);
+        _Right_Vector = new Vector2(_Speed_X, 0);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))  
+            _Rdb.velocity = _Up_Vector;
+        
+
+        if (Input.GetKey(KeyCode.S))    
+            _Rdb.velocity = _Down_Vector;
+        
+
+        if (Input.GetKey(KeyCode.A))   
+            _Rdb.velocity = _Left_Vector;
+        
+
+        if (Input.GetKey(KeyCode.D))  
+            _Rdb.velocity = _Right_Vector;
+        
+
+        if (!Input.anyKeyDown)  
+            _Rdb.velocity = Vector2.zero;
+        
+
+    }
+
+}
---
+++ Assets/Scripts/Walk_Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk_Enemy : MonoBehaviour
+{
+
+    float _Speed_X = 5;
+    float _Speed_Y = 5;
+
+    Rigidbody2D _Rdb;
+
+    Vector2 _Right_Vector;
+    Vector2 _Left_Vector;
+
+    Vector2 _Right_Up_Diagonal;
+    Vector2 _Right_Down_Diagonal;
+
+    Vector2 _Left_Up_Diagonal;
+    Vector2 _Left_Down_Diagonal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Right_Vector = new Vector2(_Speed_X, _Rdb.velocity.y);
+        _Left_Vector = new Vector2(-_Speed_X, _Rdb.velocity.y);
+
+        _Right_Up_Diagonal = new Vector2(_Speed_X, _Speed_Y);
+        _Right_Down_Diagonal = new Vector2(_Speed_X, -_Speed_Y);
+
+        _Left_Up_Diagonal = new Vector2(-_Speed_X, _Speed_Y);
+        _Left_Down_Diagonal = new Vector2(-_Speed_X, -_Speed_Y);
+
+        _Rdb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Right_Movementation()
+    {
+        _Rdb.velocity = _Right_Vector;
+    }
+
+    public void Left_Movementation()
+    {
+        _Rdb.velocity = _Left_Vector;
+    }
+
+    public void Right_Up_Diagonal()
+    {
+        _Rdb.velocity = _Right_Up_Diagonal;
+    }
+
+    public void Right_Down_Diagonal()
+    {
+        _Rdb.velocity = _Right_Down_Diagonal;
+    }
+
+    public void Left_Up_Diagonal()
+    {
+        _Rdb.velocity = _Left_Up_Diagonal;
+    }
+
+    public void Left_Down_Diagonal()
+    {
+        _Rdb.velocity = _Left_Down_Diagonal;
+    }
+
+}
